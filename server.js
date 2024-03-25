584a5ef10234f87bdc38dac9639199f883f23382
Aiden Wahed <prismhead26@gmail.com>
2024-03-24 21:16:36 -0600

    added dashboard w/ navbar & routes & hbs setup

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-
+const path = require('path')
 const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./controllers')
 
 const sequelize = require('./config/connection')
 
 const app = express()
 const PORT = process.env.PORT || 1234
+
+// TODO add helpers
+const hbs = exphbs.create({})
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(routes)
 
 
 sequelize.sync({ force: true }).then(() => {
