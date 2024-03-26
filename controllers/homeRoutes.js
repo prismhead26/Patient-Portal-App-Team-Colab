47c29db6a6fe10cb347f8304190c5b103667f0f8
Elijah Matar <m.matar515@gmail.com>
2024-03-25 21:24:45 -0500

    Created request routes for html templates

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,41 @@
-const router = require('express').Router()
-// const {  } = require('../models')
+const router = require("express").Router();
+const { Patient, Doctor } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-    try {
-        res.render('homepage')
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
-
-router.get('/login', async (req,res) => {
-    if (req.session.logged_in) {
-        return res.redirect('/');
-    }
-    res.render('/login')
+router.get("/", async (req, res) => {
+  try {
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+router.get("/login", async (req, res) => {
+  if (req.session.logged_in) {
+    return res.redirect("/");
+  }
+  res.render("/login");
+});
 
+router.get("/profile", async (req, res) => {});
 
-module.exports = router
+router.get("/patients", withAuth, async (req, res) => {
+  try {
+    const patientData = await Patient.findAll({
+      attributes: ["name"],
+      include: [{ model: Doctor }],
+      order: [["name", "ASC"]],
+    });
+
+    const patients = patientData.map((patient) => patient.get({ plain: true }));
+
+    res.render("/profile", {
+      patients,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json({ status: err, message: 'Oops, a server error!' })
+  }
+});
+
+module.exports = router;
