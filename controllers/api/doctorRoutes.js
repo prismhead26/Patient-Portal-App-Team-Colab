27c5373804c8330f7548929023fffa8fce604592
Elijah Matar <m.matar515@gmail.com>
2024-03-27 14:23:41 -0500

    Created routes for doctorRoutes file. Methods are all post for: -creating -logging in -logging out Also, sessions implemented, in each route request, to verify the status of authentication

--- controllers/api/doctorRoutes.js
+++ controllers/api/doctorRoutes.js
@@ -1,60 +1,72 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { Doctor } = require('../../models');
 
+// Create new Doctor
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const doctorData = await Doctor.create(req.body);
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.user_id = doctorData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+      res.status(200).json(doctorData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json({
+      status: 'error',
+      message: 'Oops, a server error'
+    });
   }
 });
 
+// Login Doctor
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
+    const doctorData = await Doctor.findOne({ 
+      where: 
+        { 
+          username: req.body.username,
+        } 
+    });
+    if (!doctorData) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
+    const validPassword = await doctorData.checkPassword(req.body.password);
     if (!validPassword) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
       return;
     }
-
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json({
+      status: 'error',
+      message: 'Oops, a server error'
+    });
   }
 });
 
+// Logout Doctor
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
     });
   } else {
-    res.status(404).end();
+    res.status(400).json({
+      status: 'error',
+      message: 'Oops, a server error'
+    });
   }
 });
 
