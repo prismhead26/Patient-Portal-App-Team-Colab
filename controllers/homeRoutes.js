f25deb9af685e71c54c99f55a68d61fc0cce78c9
Elijah Matar <m.matar515@gmail.com>
2024-03-27 20:49:12 -0500

    Edits to include appointment model and route for utilization on dashboard template

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,10 @@
+// ROUTES FOR HTML TEMPLATES
+
 const router = require("express").Router();
-const { Patient, Doctor } = require("../models");
+const { Patient, Doctor, Appointment } = require("../models");
 const withAuth = require("../utils/auth");
 
+// Route to get main HTML page
 router.get("/", async (req, res) => {
   try {
     res.render("homepage");
@@ -14,50 +17,56 @@ router.get("/login", async (req, res) => {
   if (req.session.logged_in) {
     return res.redirect("/");
   }
-  res.render("/login");
+  res.render("login");
 });
 
-router.get("/dashboard", async (req, res) => {
-    
+router.get("/dashboard", withAuth, async (req, res) => {
+    try {
+      const doctor = await Doctor.findByPk(req.session.id, {
+        include: [
+          {
+            model: Appointment,
+            attributes: ['time'],
+          },
+          {
+            model: Patient,
+            attributes: ['name'],
+          },
+        ],
+      });
+/*
+function utilizing script file (calendar.js) where appointment/patient 
+info will be implemented 
+*/
+      res.render('dashboard');
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: 'Oops, a server error!'
+      })
+    }
 });
 
 router.get("/profile", withAuth, async (req, res) => {
   try {
-    const patientData = await Patient.findAll({
-      attributes: ["name"],
-      include: [{ model: Doctor }],
-      order: [["name", "ASC"]],
+    const doctorData = await Doctor.findByPk(req.session.id, {
+      include: [
+        { 
+          model: Patient,
+          attributes: ['name', 'address', 'birthday'],
+        },
+      ],
     });
 
-    const patients = patientData.map((patient) => patient.get({ plain: true }));
+    const doctors = doctorData.map((doctor) => doctor.get({ plain: true }));
 
-    res.render("/profile", {
-      patients,
-      logged_in: req.session.logged_in,
+    res.render('profile', {
+        ...doctors,
     });
+
   } catch (err) {
     res.status(500).json({ status: "error", message: 'Oops, a server error!' })
   }
 });
-
-
-// // passes without auth to show data functionality
-// // very basic but retrievies doctors and their respective patients array w/ names
-// router.get("/patients", async (req, res) => {
-//   try {
-//     const doctorData = await Doctor.findAll({
-//       include: [{ model: Patient, attributes:['name']}],
-//       // order: [["name", "ASC"]],
-//     });
-
-//     const doctors = doctorData.map((doctor) => doctor.get({ plain: true }));
-//     console.log('docs....', doctors)
-//     res.render('profile', {
-//       doctors,
-//     });
-//   } catch (err) {
-//     res.status(500).json({ status: err, message: 'Oops, a server error!' })
-//   }
-// });
 
 module.exports = router;
