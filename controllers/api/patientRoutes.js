9cab653169e98ca6975526bc7fd3320c4b292ca6
Elijah Matar <m.matar515@gmail.com>
2024-03-28 21:59:25 -0500

    Created three patient routes for: creating, updating, and deleting patient information from database

--- controllers/api/patientRoutes.js
+++ controllers/api/patientRoutes.js
@@ -1,28 +1,62 @@
 const router = require("express").Router();
-const { Patient, Doctor } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Patient } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // Create a patient
-router.post('/', withAuth, async (req, res) => {
-    try {
-        const patientData = await Patient.create(req.body);
-        res.status(200).json(patientData);
-    } catch (error) {
-        res.status(500).json({
-            status: 'error',
-            message: 'Oops, an error has occured'
-        });
-    }
-});
-
-// Delete a patient
-router.delete('/:id', withAuth, async (req, res) => {
-    
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const patientData = await Patient.create(req.body);
+    res.status(200).json(patientData);
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Oops, an error has occured",
+    });
+  }
 });
 
 // Update a patient
-router.put('/:id', withAuth, async (req, res) => {
-    
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+      const patientData = await Patient.update(
+        {
+          name: req.body.name,
+          address: req.body.address,
+          phone: req.body.phone,
+          birthday: req.body.birthday,
+        },
+        {
+          where: {
+            id: req.params.id,
+            doctor_id: req.session.user_id,
+          },
+        }
+      );
+      res.status(200).json(patientData);
+    } catch (error) {
+      res.status(500).json({
+        status: "error",
+        message: "Oops, an error has occured",
+      });
+    }
+  });
+
+// Delete a patient
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const patientData = await Patient.destroy({
+      where: {
+        id: req.params.id,
+        doctor_id: req.session.user_id,
+      },
+    });
+    res.status(200).json(patientData);
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Oops, an error has occured",
+    });
+  }
 });
 
 
