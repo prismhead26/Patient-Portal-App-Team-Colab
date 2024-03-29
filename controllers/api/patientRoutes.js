20b48ede08d11e8d469ffaba552e430d1be7dad8
Elijah Matar <m.matar515@gmail.com>
2024-03-28 20:21:24 -0500

    added post route for patient database

--- controllers/api/patientRoutes.js
+++ controllers/api/patientRoutes.js
@@ -1,3 +1,29 @@
 const router = require("express").Router();
+const { Patient, Doctor } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Create a patient
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const patientData = await Patient.create(req.body);
+        res.status(200).json(patientData);
+    } catch (error) {
+        res.status(500).json({
+            status: 'error',
+            message: 'Oops, an error has occured'
+        });
+    }
+});
+
+// Delete a patient
+router.delete('/:id', withAuth, async (req, res) => {
+    
+});
+
+// Update a patient
+router.put('/:id', withAuth, async (req, res) => {
+    
+});
+
 
 module.exports = router;
