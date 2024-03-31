eec191cfe633937f128b1d5285fffbc76743db9e
Aiden Wahed <prismhead26@gmail.com>
2024-03-31 12:40:10 -0600

    code cleanup

--- public/js/appointment.js
+++ public/js/appointment.js
@@ -1,20 +1,16 @@
   const upButtonHandler = async (event) => {
     event.preventDefault();
-    console.log("button clicked");
     const title = document.querySelector("#update-appointment-title").value.trim();
     const time = document.querySelector("#update-appointment-time").value.trim();
     const patient_id = document.querySelector("#update-appointment-patient_id").value.trim();
-    console.log(title, time, patient_id);
   
     if (event.target.hasAttribute("data-id")) {
       const id = event.target.getAttribute("data-id");
-      console.log("the id is: " + id);
       const response = await fetch(`/api/appointments/${id}`, {
         method: "PUT",
         body: JSON.stringify({ title, time, patient_id }),
         headers: { "Content-Type": "application/json" },
       });
-      console.log("the response is: " + response);
       if (response.ok) {
         document.location.assign("/dashboard");
       } else {
@@ -26,7 +22,6 @@
   const delButtonHandler = async (event) => {
     if (event.target.hasAttribute("data-id")) {
       const id = event.target.getAttribute("data-id");
-      console.log(id);
       const response = await fetch(`/api/appointments/${id}`, {
         method: "DELETE",
       });
--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,7 +1,4 @@
 const user = JSON.parse(document.getElementById('calendar').dataset.user);
-// console.log(user);
-// user['appointments'].forEach((element) => console.log('time ...',element.time));
-// user['patients'].forEach((element) => console.log('patients ...',element.name));
 
 function createCalendar(data) {
     document.addEventListener('DOMContentLoaded', function() {
