b99be5158e2a06a1a08a6f8c06b2db9ef8f70c2b
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-04-02 23:26:52 -0500

    doctor can now create notes for patients with seeded notes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const helpers = require('./utils/helpers');
-require('dotenv').config()
+const helpers = require("./utils/helpers");
+require("dotenv").config();
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -20,7 +20,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {
-    maxAge: 1000 * 60 * 30, // session maxAge in milliseconds
+    maxAge: 1000 * 60 * 30 * 24, // session maxAge in milliseconds
     httpOnly: true, // if true: prevents client side JS from reading the cookie
     secure: false, // if true: only transmit cookie over https
     sameSite: "strict",
