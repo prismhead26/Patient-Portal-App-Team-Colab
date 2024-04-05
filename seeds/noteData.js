e59d7431797cfd930b2c481993828e71a050852c
Elijah Matar <m.matar515@gmail.com>
2024-04-04 19:28:32 -0500

    uncomment notes

--- seeds/noteData.js
+++ seeds/noteData.js
@@ -1,63 +1,63 @@
-// const { Note } = require("../models");
+const { Note } = require("../models");
 
-// const noteData = [
-//   {
-//     body: "This is a note",
-//     patient_id: 1,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 2,
-//   },
-//   {
-//     body: "This is another note",
-//     patient_id: 2,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 4,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 6,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 8,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 10,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 11,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 12,
-//   },
-//   {
-//     body: "Fall Risk",
-//     patient_id: 12,
-//   },
-//   {
-//     body: "New medicine prescribed",
-//     patient_id: 12,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 14,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 16,
-//   },
-//   {
-//     body: "This is a note",
-//     patient_id: 17,
-//   },
-// ];
+const noteData = [
+  {
+    body: "This is a note",
+    patient_id: 1,
+  },
+  {
+    body: "This is a note",
+    patient_id: 2,
+  },
+  {
+    body: "This is another note",
+    patient_id: 2,
+  },
+  {
+    body: "This is a note",
+    patient_id: 4,
+  },
+  {
+    body: "This is a note",
+    patient_id: 6,
+  },
+  {
+    body: "This is a note",
+    patient_id: 8,
+  },
+  {
+    body: "This is a note",
+    patient_id: 10,
+  },
+  {
+    body: "This is a note",
+    patient_id: 11,
+  },
+  {
+    body: "This is a note",
+    patient_id: 12,
+  },
+  {
+    body: "Fall Risk",
+    patient_id: 12,
+  },
+  {
+    body: "New medicine prescribed",
+    patient_id: 12,
+  },
+  {
+    body: "This is a note",
+    patient_id: 14,
+  },
+  {
+    body: "This is a note",
+    patient_id: 16,
+  },
+  {
+    body: "This is a note",
+    patient_id: 17,
+  },
+];
 
-// const seedNote = () => Note.bulkCreate(noteData);
-// module.exports = seedNote;
+const seedNote = () => Note.bulkCreate(noteData);
+module.exports = seedNote;
